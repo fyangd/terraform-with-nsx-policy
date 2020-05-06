bb715a36b1c7952049f055544bdce54dd03ad282
Dumlu Timuralp <34840364+dumlutimuralp@users.noreply.github.com>
2020-05-06 18:43:06 +0100

    Create variables.tf

---
+++ iteration1/variables.tf
@@ -0,0 +1,48 @@
+variable "nsx_manager_ip" {
+    default = "10.79.1.10"
+}
+
+variable "nsx_username" {
+  default = "admin"
+}
+ 
+variable "nsx_password" {
+    default = "VMware1!VMware1!"
+}
+
+variable "overlay_tz" {
+    # type = string
+    default = "nsx-overlay-transportzone"
+}
+
+variable "vlan_tz" {
+    # type = string
+    default = "nsx-vlan-transportzone"
+}
+
+variable "edge_node_first" {
+    default = "edge03"
+}
+variable "edge_node_second" {
+    default = "edge04"
+}
+
+variable "edge_cluster" {
+    default = "EdgeCluster2"
+}
+
+variable "vlan_uplink_segment" {
+    default = "uplink_segment"
+}
+
+variable "tier0" {
+    default = "tier0_demo"
+}
+
+variable "tier1" {
+    default = "tier1_demo"
+}
+
+variable "tkg_segment" {
+    default = "tkg_overlay_segment" 
+}
