21e975df95605874f515553ae758612b68c54294
Automation User <kubo@jumper.localdomain>
2020-05-07 07:32:17 -0700

    a minimal CAPV test framework for verifying NSX compatibility.
    cleanup

---
+++ iteration-tkg/main.tf
@@ -0,0 +1,188 @@
+##################################################
+##################################################
+# Existing NSX objects to pull
+##################################################
+##################################################
+
+data "nsxt_policy_transport_zone" "overlay_tz" {
+    display_name         = var.nsx_vars.overlay_tz
+}
+ 
+data "nsxt_policy_transport_zone" "vlan_tz" {
+    display_name         = var.nsx_vars.vlan_tz
+}
+ 
+data "nsxt_policy_edge_cluster" "edge_cluster" {
+    display_name         = var.nsx_vars.edge_cluster
+}
+
+data "nsxt_policy_edge_node" "edge_node_first" {
+    edge_cluster_path    = data.nsxt_policy_edge_cluster.edge_cluster.path
+    display_name         = var.nsx_vars.edge_node_first
+}
+
+##################################################
+##################################################
+# Resources that will be created by Terraform
+##################################################
+##################################################
+
+# VLAN segment (for Tier 0 Uplink)
+
+resource "nsxt_policy_vlan_segment" "uplink_vlan" {
+    display_name           = var.nsx_vars.vlan_uplink_segment_name
+    description            = "Uplink of Tier0 provisioned by Terraform"
+    vlan_ids               = [var.nsx_vars.vlan_uplink_segment_vlanid]
+    transport_zone_path    = data.nsxt_policy_transport_zone.vlan_tz.path
+
+    tag {
+        scope = "demo"
+        tag   = "tftest"
+    }
+
+}
+
+
+# Tier 0 
+
+resource "nsxt_policy_tier0_gateway" "tier0_new" {
+    display_name              = var.nsx_vars.tier0_name
+    description               = "Tier-0 provisioned by Terraform"
+    failover_mode             = "NON_PREEMPTIVE"
+    default_rule_logging      = false
+    enable_firewall           = false
+    #force_whitelisting        = true
+    ha_mode                   = "ACTIVE_STANDBY"
+    edge_cluster_path         = data.nsxt_policy_edge_cluster.edge_cluster.path
+ 
+    tag {
+        scope = "demo"
+        tag   = "tftest"
+    }
+}
+
+# Attach VLAN segment to Tier0 on Edge Node First
+
+resource "nsxt_policy_tier0_gateway_interface" "uplink1edgefirst" {
+    display_name        = "Uplink1EdgeFirst"
+    type                = "EXTERNAL"
+    edge_node_path      = data.nsxt_policy_edge_node.edge_node_first.path
+    gateway_path        = nsxt_policy_tier0_gateway.tier0_new.path
+    segment_path        = nsxt_policy_vlan_segment.uplink_vlan.path
+    subnets             = [var.nsx_vars.tier0_uplink_edge_node_first_ip]
+    mtu                 = 1500
+
+    tag {
+        scope = "demo"
+        tag   = "tftest"
+    }
+
+}
+
+# Attach VLAN segment to Tier0 on Edge Node Second
+# Default static route on Tier 0
+
+resource "nsxt_policy_static_route" "default_route" {
+    display_name = "default"
+    gateway_path = nsxt_policy_tier0_gateway.tier0_new.path
+    network      = "0.0.0.0/0"
+
+    next_hop {
+        admin_distance = "1"
+        ip_address     = var.nsx_vars.tier0_default_route_next_hop_ip
+    }
+
+    tag {
+        scope = "demo"
+        tag   = "tftest"
+    }
+}
+
+# DHCP Server Profile for Tier 1
+
+resource "nsxt_policy_dhcp_server" "tier1_dhcp" {
+    display_name     = "DhcpServer"
+    description      = "DHCP server for the Tier1"
+    edge_cluster_path = data.nsxt_policy_edge_cluster.edge_cluster.path
+    server_addresses = ["100.94.0.1/30"]
+    # This DHCP server IP is locally significant for the Tier1
+
+    tag {
+        scope = "demo"
+        tag   = "tftest"
+    }
+}
+
+
+# Tier 1
+
+resource "nsxt_policy_tier1_gateway" "tier1_new" {
+    display_name                = var.nsx_vars.tier1_name
+    description                 = "Tier-1 provisioned by Terraform"
+    # edge_cluster_path         = data.nsxt_policy_edge_cluster.edge_cluster.path
+    # failover_mode             = "NON_PREEMPTIVE"
+    # default_rule_logging      = "false"
+    # enable_firewall           = "true"
+    # enable_standby_relocation = "false"
+    # force_whitelisting        = "true"
+    dhcp_config_path            = nsxt_policy_dhcp_server.tier1_dhcp.path
+    tier0_path                  = nsxt_policy_tier0_gateway.tier0_new.path
+    route_advertisement_types   = ["TIER1_CONNECTED"]
+ 
+    tag {
+        scope = "demo"
+        tag   = "tftest"
+    }
+ 
+    # route_advertisement_rule {
+    #    name                      = "Tier 1 Networks"
+    #    action                    = "PERMIT"
+    #    subnets                   = ["10.10.10.0/24", "10.20.20.0/24"]
+    #    prefix_operator           = "GE"
+    #    route_advertisement_types = ["TIER1_CONNECTED"]
+    #}
+}
+
+# Segment 1 on Tier 1 (with DHCP enabled)
+
+resource "nsxt_policy_segment" "tier1_segment1" {
+    display_name        = var.nsx_vars.tier1_segment1_name
+    description         = "Segment created by Terraform"
+    transport_zone_path = data.nsxt_policy_transport_zone.overlay_tz.path
+    connectivity_path   = nsxt_policy_tier1_gateway.tier1_new.path
+
+    tag {
+        scope = "demo"
+        tag   = "tftest"
+    }
+ 
+    subnet {   
+        cidr        = var.nsx_vars.tier1_segment1_ip
+        dhcp_ranges = [var.nsx_vars.tier1_segment1_dhcp_range] 
+     
+   
+        dhcp_v4_config {
+             lease_time  = 36000
+             dns_servers = [var.nsx_vars.tier1_segment1_dns_server_ip]
+        }
+    }
+}
+
+resource "null_resource" "ubuntu" {
+
+  connection {
+    type = "ssh"
+	  agent = "false"
+	  host = "127.0.0.1"
+      
+	  user = "kubo"
+	  password = "Ponies!23"
+  }
+
+  provisioner "remote-exec" {
+
+    inline = [
+       var.nsx_vars.bootstrap
+    ]
+  }
+}
---
+++ iteration-tkg/variables.tf
@@ -0,0 +1,25 @@
+variable "nsx_vars" {
+    type = map(string)
+    default = {
+        overlay_tz = "tz-overlay"
+        vlan_tz = "internet-tz-vlan-0"
+        edge_cluster = "edge-cluster-0"
+        edge_node_first = "tn-cluster-0-edge-0"
+
+        vlan_uplink_segment_name = "uplink_segment"
+        vlan_uplink_segment_vlanid = "0"
+        tier0_name = "tier0-demo"
+
+        tier0_uplink_edge_node_first_ip="192.168.111.123/24"
+        tier0_default_route_next_hop_ip="192.168.111.155"
+
+        tier1_name = "tier1-demo"
+
+        tier1_segment1_name="workload_segment1"
+        tier1_segment1_ip="12.10.10.1/24"
+        tier1_segment1_dhcp_range="12.10.10.51-12.10.10.99"
+        tier1_segment1_dns_server_ip="192.168.111.155"
+        
+        bootstrap = "sudo ip route delete 12.10.10.0/24 ; echo Ponies!23 | sudo -S route add -net 12.10.10.0/24 gw 192.168.111.123" 
+    }
+}
