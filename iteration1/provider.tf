b468a3ae82b6ee8c52f05e862b06155bb435f550
Dumlu Timuralp <34840364+dumlutimuralp@users.noreply.github.com>
2020-05-07 14:42:16 +0100

    Update provider.tf

--- iteration1/provider.tf
+++ iteration1/provider.tf
@@ -1,7 +1,7 @@
 provider "nsxt" {
-    host                     = var.nsx_vars.nsx_manager_ip
-    username                 = var.nsx_vars.nsx_manager_username
-    password                 = var.nsx_vars.nsx_manager_password
+    host                     = var.nsx_manager_ip
+    username                 = var.nsx_username
+    password                 = var.nsx_password
     allow_unverified_ssl     = true
     max_retries              = 10
     retry_min_delay          = 500
