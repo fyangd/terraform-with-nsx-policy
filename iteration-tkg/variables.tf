c5760445047009c0724d30d171ac15e397c16709
Fei Yang <yangfe@vmware.com>
2020-05-15 18:26:32 -0700

    Update terraform variable
    Authored-by: Fei Yang <yangfe@vmware.com>

--- iteration-tkg/variables.tf
+++ iteration-tkg/variables.tf
@@ -7,19 +7,18 @@ variable "nsx_vars" {
         edge_node_first = "tn-cluster-0-edge-0"
 
         vlan_uplink_segment_name = "uplink_segment"
-        vlan_uplink_segment_vlanid = "0"
-        tier0_name = "tier0-demo"
+        vlan_uplink_segment_vlanid = "101"
 
-        tier0_uplink_edge_node_first_ip="192.168.111.123/24"
-        tier0_default_route_next_hop_ip="192.168.111.155"
+        tier0_name = "tier0-demo"
+        tier0_uplink_edge_node_first_ip="192.168.115.123/24"
+        tier0_default_route_next_hop_ip="192.168.115.1"
 
         tier1_name = "tier1-demo"
-
         tier1_segment1_name="workload_segment1"
         tier1_segment1_ip="12.10.10.1/24"
         tier1_segment1_dhcp_range="12.10.10.51-12.10.10.99"
-        tier1_segment1_dns_server_ip="192.168.111.155"
-        
-        bootstrap = "sudo ip route delete 12.10.10.0/24 ; echo Ponies!23 | sudo -S route add -net 12.10.10.0/24 gw 192.168.111.123" 
+        tier1_segment1_dns_server_ip="192.168.115.155"
+
+        bootstrap = "sudo ip route delete 12.10.10.0/24 ; echo Ponies!23 | sudo -S route add -net 12.10.10.0/24 gw 192.168.115.123"
     }
 }
